11ed9bba0439db97d15d6924e61188f2e8b72135
Lauren <Laurenelee88@gmail.com>
2018-01-04 09:14:14 -0800

    brainstorming what db will look like

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+include faker
+
+20.times do
+  AgencyDetail.create(
+
+  )
+end
+######### for agency name:
+# Faker::App.name
+# or company name ?
+
+######### for age range:
+# .between(from = 1.00, to = 5000.00) ⇒ Object
+
+######### for volunteer type (not faker but a potential list like this): 
+# seniors, homeless, arts and culture, animals, advocacy and human rights, children and youth, computers and technology, community, education and literacy, health and medicine, disaster relief, LGBT, justice and legal, environment, faith-based, crisis support, immigrants and refugees, sports and recreation
+
+######### for address location
+# Faker::Address.latitude
+# Faker::Address.longitude
